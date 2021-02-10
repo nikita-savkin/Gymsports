a4a733184cdcb3154405c2c5d5f2dfa89d91a71c
Nikita <no.colour.you.like@gmail.com>
2021-02-10 20:34:18 +0300

    trainer carousel

--- js/script.js
+++ js/script.js
@@ -1,2 +1,13 @@
-'use strict'
+// Тренеры - карусель
+  $('.trainers__slider').slick({
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow: '<button type="button" class="slick-prev slick-arrow__trainers slick-prev__trainers"></button>',
+    nextArrow: '<button type="button" class="slick-next slick-arrow__trainers slick-next__trainers"></button>'
+  });
+
+
+
+
 
